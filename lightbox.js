44d924f9e4080b35772c8d58b1666f6b3e1de02c
talrez11 <tal@way2cu.com>
2015-10-10 22:49:25 +0300

    Added lightbox object functions

--- lightbox.js
+++ lightbox.js
@@ -5,11 +5,38 @@
 
 function lighthbox( container )  {
 	var self = this;
-	
+
 	// Initialize object
 	self.init = function() {
 
 	}
+
+	// Create light box container element
+	self.lightbox_container = function() {
+
+	}
+
+	// Finction for handling item click 
+	self.handle_image = function() {
+
+	}
+
+	// Function displaying next image in lightbox
+	self.show_next = function() {
+
+	}
+
+	// Function displaying previous image in lightbox
+	self.show_next = function() {
+		
+	}
+
+	// Function returning index of image in array
+	self.image_position = function() {
+		
+	}
+
+	
 
 	// Initialize object
 	self.init();
